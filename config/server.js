fec610d4ec9a4a96a97bed591540bdb361f7791e
james <jmspaul101@gmail.com>
2021-07-10 04:36:59 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e4dd295c1771caddc7a112e325157c08'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1f548be2-c6ea-42d0-80ef-e1e78546b2b1'
+};
